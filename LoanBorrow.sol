ed436076de4aad0407a653a35208fe3f5b3f4c69
KP08082132 <119520071+KP08082132@users.noreply.github.com>
2022-12-23 23:50:35 +0530

    Update LoanBorrow.sol

--- LoanBorrow.sol
+++ LoanBorrow.sol
@@ -1,40 +1,49 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >0.7.0 <0.9.0;
 
-contract SmartBankAccount {
-
+contract lendingP2P {
+    address payable sender;
+    address payable receiver;
     uint totalContractBalance = 0;
-
-    function getContractBalance() public view returns(uint){
+    uint totalContractBalanceReceiver = 0;
+    
+    function getContractBalance(address person) public view returns(uint){
+        totalContractBalance = person.balance; 
         return totalContractBalance;
+
     }
+    
     
     mapping(address => uint) balances;
     mapping(address => uint) depositTimestamps;
     
-    function addBalance() public payable {
-        balances[msg.sender] = msg.value;
-        totalContractBalance = totalContractBalance + msg.value;
-        depositTimestamps[msg.sender] = block.timestamp;
+    function sendLoan( address payable receiver) public payable {
+        
+        if(msg.value < getContractBalance(sender))
+        {
+            receiver.transfer(msg.value);
+            depositTimestamps[msg.sender] = block.timestamp;
+        }
+        
     }
     
-    function getBalance(address userAddress) public view returns(uint) {
-        uint principal = balances[userAddress];
-        uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; //seconds
-        return principal + uint((principal * 7 * timeElapsed) / (100 * 365 * 24 * 60 * 60)) + 1; //simple interest of 0.07%  per year
-    }
-    
-    function withdraw(uint withdrawAmount) public payable {
+     
+    /*function withdraw(uint withdrawAmount) public payable {
         address payable withdrawTo = payable(msg.sender);
         uint amountToTransfer = withdrawAmount*1000000000000000000;
         withdrawTo.transfer(amountToTransfer);
         totalContractBalance = totalContractBalance - amountToTransfer;
         balances[msg.sender] -= amountToTransfer;
-    }
+    }*/
     
-    function addMoneyToContract() public payable {
-        totalContractBalance += msg.value;
+    function returnLoan(address payable sender) public payable {
+    uint timeElapsed = block.timestamp - depositTimestamps[receiver]; //seconds
+    
+     sender.transfer(msg.value + uint((msg.value * 7 * timeElapsed) / (100 * 365 * 24 * 60 * 60)) + 1);
     }
+    function deposit() public payable {}
+
+    
 
     
 }
